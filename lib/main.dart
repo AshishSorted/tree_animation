02eb1052bf86dd5f2d72440cb9511049d03ed143
AkashTripathi4 <akash.tripathi@sorted.team>
2024-03-15 22:46:48 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,14 @@ import 'my_app.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
-  
-   runApp(const MyApp());
+  initLogger(() async {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle());
+
+    await _main();
+  });
 }
 
-
+Future<void> _main() async {
+  // SidebarController sidebarController = Get.put(SidebarController());
+  runApp(const MyApp());
+}
